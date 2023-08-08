4a5889e06919cd98066528327f2c985ac77dcee6
Karennn-Y <113325061+Karennn-Y@users.noreply.github.com>
2023-08-08 22:29:49 +0900

    Add files via upload

---
+++ chapter6/swap.c
@@ -0,0 +1,22 @@
+/* In file name swap.c */
+
+#include <stdio.h>
+
+void swap (int *i, int *j)
+{
+	int tmp;
+
+	tmp = *i;
+	*i = *j;
+	*j = tmp;
+}
+
+int main (int argc, char **argv)
+{
+	int i = 3, j = 5;
+
+	swap(&i, &j);
+	printf("%d %d\n", i, j);   
+
+	return 0;
+}
